fbede2e82b69de02bee22629ede0a53cd0420fbe
Labib909 <136590675+Labib909@users.noreply.github.com>
2026-08-15 10:53:26 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyiyAUAq_frmRgJ7FL6t-mXvHiiTZU-FNQq7cHiSrfvOGeL5D6oKTdPMY_HnmaS6_m9/execURL_HERE";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyiyAUAq_frmRgJ7FL6t-mXvHiiTZU-FNQq7cHiSrfvOGeL5D6oKTdPMY_HnmaS6_m9/exec";
